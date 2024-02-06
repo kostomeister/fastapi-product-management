28e1a55ee5e5eca0634e378556d5594dc150f5fd
Kostia <kostia3507@gmail.com>
2024-02-06 19:24:35 +0200

    created sqlalchemy repo

--- src/utils/repository.py
+++ src/utils/repository.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 
+from sqlalchemy import select
+
+from src.db.db import SessionLocal
+
 
 class AbstractRepository(ABC):
 
@@ -22,3 +26,53 @@ class AbstractRepository(ABC):
     @abstractmethod
     async def delete_one(self, id: int):
         pass
+
+
+class SQLAlchemyRepository(AbstractRepository):
+    model = None
+
+    async def get_one(self, id: int):
+        async with SessionLocal() as db:
+            query = select(self.model).where(self.model.id == id)
+            result = await db.execute(query)
+            return result.scalar_one()
+
+    async def get_all(self):
+        async with SessionLocal() as db:
+            query = select(self.model)
+            result = await db.execute(query)
+            return result.scalars().all()
+
+    async def create_one(self, data: dict):
+        print(data)
+        async with SessionLocal() as db:
+            new_object = self.model(**data)
+            db.add(new_object)
+            await db.commit()
+            return new_object
+
+    async def update_one(self, id: int, data: dict):
+        async with SessionLocal() as db:
+            query = select(self.model).where(self.model.id == id)
+            result = await db.execute(query)
+            obj = await result.fetchone()
+
+            if obj:
+                for key, value in data.items():
+                    setattr(obj, key, value)
+                await db.commit()
+                return obj
+            else:
+                return None
+
+    async def delete_one(self, id: int):
+        async with SessionLocal() as db:
+            query = select(self.model).where(self.model.id == id)
+            result = await db.execute(query)
+            obj = await result.fetchone()
+
+            if obj:
+                db.delete(obj)
+                await db.commit()
+            else:
+                return None
