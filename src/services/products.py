53c62a61ca9553f9c4d766c76dc77c1bfa2016cf
Kostia <kostia3507@gmail.com>
2024-02-25 21:44:24 +0200

    added delete & update methods to product service

--- src/services/products.py
+++ src/services/products.py
@@ -1,4 +1,4 @@
-from src.schemas.products import ProductCreate
+from src.schemas.products import ProductCreate, ProductUpdate
 from src.utils.repository import AbstractRepository
 
 
@@ -17,3 +17,10 @@ class ProductService:
     async def get_products(self):
         return await self.product_repo.get_all()
 
+    async def delete_product(self, id: int):
+        await self.product_repo.delete_one(id)
+        return {"message": f"product with id {id} deleted successfully"}
+
+    async def update_product(self, id: int, data: ProductUpdate):
+        product_dict = data.model_dump()
+        return await self.product_repo.update_one(id, product_dict)
